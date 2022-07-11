454b5abae5b28dfdd1a1335fe2628a92bf267876
JosueRod21 <josue_rod21@hotmail.com>
2022-07-11 13:12:32 -0500

    Version 2.0

--- generadorpwm.v
+++ generadorpwm.v
@@ -1,22 +1,25 @@
-module generadorpwm(rst, clk, rst_syn, e, pwmin, pwmout, carry);
-	parameter widthCount = 14, cntMaxCount = 10000;
-	input rst, clk,rst_syn, e;
-	input [widthCount-1:0]pwmin; //
-	output reg pwmout; 
+module generadorpwm(reset, clock, rst_syn, e, pwm_in, pwm_out, carry);
+	parameter Ancho_contador = 14, contador_pulsos_pwm = 10000;
+
+	input reset, clock,rst_syn, e;
+	input [Ancho_contador-1:0]pwm_in; //
+
+	output reg pwm_out; 
 	output carry;
-	wire [widthCount-1:0]cnt;
+
+	wire [Ancho_contador-1:0]cuenta_pwm;
 	
 	
-	Counter #(widthCount,cntMaxCount,0) counter(
-		.rst(rst), 
-		.clk(clk), 
-		.rst_syn(rst_syn),	
-		.e(e), 
-		.cnt(cnt), 
-		.carry(carry));
+	Counter #(Ancho_contador,contador_pulsos_pwm,0) counter(
+		.reset(reset),
+		.clock(clock),
+		.rst_syn(rst_syn),
+		.e(e),
+		.carry(carry),
+		.salida_cont(cuenta_pwm));
 		
-	always @(pwmin, cnt, rst_syn)
-		if (rst_syn) pwmout <= 1'b0;
-		else if(cnt < pwmin) pwmout <= 1'b1;
-		else pwmout <= 1'b0;
+	always @(pwm_in, cuenta_pwm, rst_syn)
+		if (rst_syn) pwm_out <= 1'b0;
+		else if(cuenta_pwm < pwm_in) pwm_out <= 1'b1;
+		else pwm_out <= 1'b0;
 endmodule
